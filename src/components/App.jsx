a700d0c3127b8e52ec6ccb5a1dd660e554aa81ca
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-04 23:54:05 +0000

    added mouse button style change function

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [heading, setHeading] = useState("hello");
+  const [isMousedOver, setMousedOver] = useState(false);
+
+  function handleClick(){
+    setHeading("Submitted");
+  }
+
+  function handleMouseOver(){
+    setMousedOver(true)
+  }
+  function handleMouseOut(){
+    setMousedOver(false)
+  }
+
+  return (
+    <div className="container">
+      <h1>{heading}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button 
+      style={{backgroundColor: isMousedOver ? "black":"white"}}
+      onClick={handleClick} 
+      onMouseOver={handleMouseOver} 
+      onMouseOut={handleMouseOut} 
+       >Submit</button>
+    </div>
+  );
+}
+
+export default App;
